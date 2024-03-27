e1a93204348b5024ab1487e40ababb6a4daa2a56
henryfogacas <henryfogacas@gmail.com>
2024-03-27 13:15:50 -0300

    Teste

--- cypress/e2e/.nomedoteste.cy.js
+++ cypress/e2e/.nomedoteste.cy.js
@@ -17,4 +17,7 @@ describe('Agrupa os test cases', () => {
      cy.visit('/Menu')
 //não é preciso fazer logout pois o comando cy.session apaga e restaura automaticamente a sessão ativa
   });
+  it('Teste', () => {
+    
+  });
 });
